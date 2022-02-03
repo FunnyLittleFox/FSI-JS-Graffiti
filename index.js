f16990e789d13a9673027ceb6a61c52aab691c32
Fox Flaherty <katyra14@gmail.com>
2022-02-02 20:00:35 -0800

    completed base of assignment still need to experiment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+header.style.fontFamily = "papyrus"
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+}
+let dogDescription = document.querySelectorAll('.dog-description')
+
+let footer = document.querySelector('.footer')
+footer.style.fontFamily = "papyrus"
+footer.style.backgroundColor = "Purple"
+footer.style.color = "yellow"
+
+
